e850c57fc3150f40b23152b7e3c30354bd9bd8df
Bryan Maldonado <bmaldonadoi1@est.ups.edu.ec>
2026-01-20 13:04:25 -0500

    Archivos de configuración y main

---
+++ main.py
@@ -0,0 +1,9 @@
+from genera_dataset.dataset_rps import generar_datos_rps as generar_dataset_rps
+
+def main():
+    print("Generando dataset procesado...")
+    generar_dataset_rps()
+    print("Dataset generado correctamente")
+
+if __name__ == "__main__":
+    main()
