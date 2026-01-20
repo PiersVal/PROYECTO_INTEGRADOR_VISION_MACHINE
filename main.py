dd0e7f35a8e1312e30a66dd3d8e390e166426a41
franciscooca22 <franciscoocana22@gmail.com>
2026-01-20 14:14:45 -0500

    Se modificó el archivo main para integrar el dataset y procesamiento de animales

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from genera_dataset.dataset_rps import generar_datos_rps as generar_dataset_rps
+from genera_dataset.dataset_animales import generar_datos_animales as generar_dataset_animales
+
 
 def main():
-    print("Generando dataset procesado...")
+    print("Generando dataset RPS...")
     generar_dataset_rps()
-    print("Dataset generado correctamente")
+    print("Dataset RPS generado\n")
+
+    print("Generando dataset de animales (YOLO + pipeline clásico)...")
+    generar_dataset_animales()
+    print("Dataset de animales generado correctamente")
+
 
 if __name__ == "__main__":
     main()
