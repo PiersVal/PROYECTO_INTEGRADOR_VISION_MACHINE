3a46e4a2d0c697a786efb9003e6db3ca03c51d1f
Bryan Maldonado <bmaldonadoi1@est.ups.edu.ec>
2026-01-21 11:36:25 -0500

    Actualización archivo main

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from genera_dataset.dataset_rps import generar_datos_rps as generar_dataset_rps
 from genera_dataset.dataset_animales import generar_datos_animales as generar_dataset_animales
 
 
 def main():
-    print("Generando dataset RPS...")
-    generar_dataset_rps()
-    print("Dataset RPS generado\n")
 
     print("Generando dataset de animales (YOLO + pipeline clásico)...")
     generar_dataset_animales()
