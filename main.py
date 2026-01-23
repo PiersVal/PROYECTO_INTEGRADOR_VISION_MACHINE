22c03adcca6ac252efbff0a40f334cb99cd27cca
franciscooca22 <franciscoocana22@gmail.com>
2026-01-23 01:04:03 -0500

    Actualización en el archivo main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src.features.procesamiento_vehiculos import procesar_datasets as generar_dataset_vehiculos
+from src.features.procesamiento_animales import procesar_datasets as generar_dataset_animales
+
 
 def main():
 
@@ -6,5 +8,11 @@ def main():
     generar_dataset_vehiculos()
     print("Dataset de vehiculos generado correctamente")
 
+    print("Generando dataset de Animales")
+    generar_dataset_animales()
+    print("Dataset de animales generado correctamente")
+
+
 if __name__ == "__main__":
     main()
+    
