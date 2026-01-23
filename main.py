6885b0acfab613b050889d5884cc43143430bce6
Bryan Maldonado <bmaldonadoi1@est.ups.edu.ec>
2026-01-23 00:08:58 -0500

    Actualización archivo main

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from genera_dataset.dataset_animales import generar_datos_animales as generar_dataset_animales
-
+from src.features.procesamiento_vehiculos import procesar_datasets as generar_dataset_vehiculos
 
 def main():
 
-    print("Generando dataset de animales (YOLO + pipeline clásico)...")
-    generar_dataset_animales()
-    print("Dataset de animales generado correctamente")
-
+    print("Generando dataset de Vehiculos")
+    generar_dataset_vehiculos()
+    print("Dataset de vehiculos generado correctamente")
 
 if __name__ == "__main__":
     main()
